afbe5c0c71b66f4a600db28abaf025322752e457
youssefmac <youssef@mac.com>
2025-03-19 23:19:03 +0200

    added htmlnode and htmlnode tests

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNODE:
+    def __init__(self, tag =None , value=None, children=[], props={}):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError("This method should be overriden by the subclass")
+    
+    def props_to_html(self):
+        for k,v in self.props.items():
+            return f'{k}="{v}" '
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
---
+++ src/test_htmlnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from htmlnode import *
+
+class TestHtmlNode(unittest.TestCase):
+    def test_init(self):
+        node = HTMLNODE('div')
+        self.assertEqual(node.tag, 'div')
+        self.assertEqual(node.value, None)
+        self.assertEqual(node.children, [])
+        self.assertEqual(node.props, {})
+    
+    def test_repr(self):
+        node = HTMLNODE('div')
+        self.assertEqual(repr(node), "HTMLNode(div, None, [], {})")
+    
+    def test_props_to_html(self):
+        node = HTMLNODE('div', props={'class':'container'})
+        self.assertEqual(node.props_to_html(), 'class="container" ')
+
+
+
+if __name__ == "__main__":
+    unittest.main()
