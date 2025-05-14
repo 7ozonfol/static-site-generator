52b5d60d5231b83d79bf97f89ab2b69a6deb8bc6
youssefmac <youssef@mac.com>
2025-05-14 20:01:15 +0300

    added block types and test

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,13 @@
+from enum import Enum
 from textnode import TextNode, TextType
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDEREDLIST = "unorderedlist"
+    ORDEREDLIST = "orderedlist"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -9,3 +18,17 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(block):
+    if block.startswith("# "):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith("> "):
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        return BlockType.UNORDEREDLIST
+    elif block[0].isdigit() and block[1] == ".":
+        return BlockType.ORDEREDLIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -46,6 +46,13 @@ This is the same paragraph on a new line
             ],
         )
 
+    def test_block_to_block_type(self):
+        self.assertEqual(block_to_block_type("# Heading"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("```code```"), BlockType.CODE)
+        self.assertEqual(block_to_block_type("> Quote"), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type("- List item"), BlockType.UNORDEREDLIST)
+        self.assertEqual(block_to_block_type("1. Ordered item"), BlockType.ORDEREDLIST)
+        self.assertEqual(block_to_block_type("Normal text"), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
